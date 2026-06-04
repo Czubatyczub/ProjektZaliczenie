67a4784690df9d36cfce700a6f1c02f58b5f83ea
Czubatyczub <seviper@onet.pl>
2026-06-04 14:06:34 +0200

    aktualizacja 0.2

--- Tester_mocy_hasel.py
+++ Tester_mocy_hasel.py
@@ -9,12 +9,59 @@
 
 # pobieranie hasła od użytkownika
 print("Podaj proszę swoje hasło a ja określę czy jest wystarczająco silne")
-hasło_uzytkownika = str(input("Twoje hasło: "))
+while True:
+ hasło_uzytkownika = str(input("Twoje hasło: "))
 
-# 2 sprawdzenie długości hasla
-def sprdlugoscihasla():
+ punkty_mocy = 0
+ # 2 sprawdzenie długości hasla
+ def sprdlugoscihasla():
     dlugosc_hasla = len(hasło_uzytkownika)
     if dlugosc_hasla < 8:
         print("twoje hasło jest zbyt krótkie, musi się składać z minimalnie 8 znaków")
     return dlugosc_hasla
+
+ if sprdlugoscihasla() < 8:
+   continue
+
+ #jeżeli warunek jest spełniony to dodaj moc 1 haśle
+ punkty_mocy += 1
+
+
+ def sprznaki():
+    wynik_duzych_liter = 0
+    # pętla sprawdzająca wiekosc liter
+    for literka in hasło_uzytkownika:
+        if literka == literka.upper():
+            wynik_duzych_liter += 1
+    if wynik_duzych_liter < 2:
+        return print("wprowadź minimalnie 2 duży litery w haśle!")
+
+
+ #sprawdzenie wielości liter
+
+ def wielosc_liter():
+    ma_małą = 0
+    ma_dużą = 0
+
+    #pętla sprawdzająca wiekosc liter
+    for literka in hasło_uzytkownika:
+        if literka.isupper():
+           ma_dużą += 1
+
+        if literka.islower():
+            ma_małą += 1
+    if ma_dużą < 2:
+        print("wprowadź minimalnie 2 duży litery w haśle!")
+        print("musisz wpisać poprawne hasło")
+        sprdlugoscihasla()
+        return False
+    return True
+ if wielosc_liter():
+    punkty_mocy += 1
+ else:
+    continue
+ break
+
+
+
 
